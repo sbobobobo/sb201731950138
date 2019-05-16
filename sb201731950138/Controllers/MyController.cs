23d8e485d4329a5c8997520704e252959661b2b3
Administrator <Administrator@6-206-A21>
2019-05-16 11:25:34 +0800

    添加项目文件。

---
+++ sb201731950138/Controllers/MyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sb201731950138.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ sb201731950138/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sb201731950138.MvcApplication" Language="C#" %>
